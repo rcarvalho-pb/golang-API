a6a3cc487803be6350d50bc6e44af5eb625a4a24
Ramon Carvalho <rcarvalho.pb@gmail.com>
2023-01-01 17:29:57 -0300

    Implemented function create user with repositories

--- API/src/controllers/usuarios.go
+++ API/src/controllers/usuarios.go
@@ -5,6 +5,7 @@ import (
 	"api/src/modelos"
 	"api/src/repositorios"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -27,7 +28,12 @@ func CriarUsuario(w http.ResponseWriter, r *http.Request){
 	}
 
 	repositorio := repositorios.NovoRepositorioDeUsuario(db)
-	repositorio.Criar(usuario)
+	usuarioID, erro := repositorio.Criar(usuario)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	w.Write([]byte(fmt.Sprintf("ID inserido: %d", usuarioID)))
 
 }
 
--- API/src/repositorios/usuarios.go
+++ API/src/repositorios/usuarios.go
@@ -14,6 +14,31 @@ func NovoRepositorioDeUsuario (db *sql.DB) *usuarios{
 	return &usuarios{db}
 }
 
-func (u usuarios) Criar(usuario modelos.Usuario) (uint64, error) {
-	return 0, nil
+func (repositorio usuarios) Criar(usuario modelos.Usuario) (uint64, error) {
+	statement, erro := repositorio.db.Prepare(
+		"INSERT INTO usuarios (nome, nick, email, senha) VALUES (?, ?, ?, ?)",
+	)
+	if erro != nil {
+		return 0, erro
+	}
+
+	defer statement.Close()
+	
+	resultado, erro := statement.Exec(
+		usuario.Nome,
+		usuario.Nick,
+		usuario.Email,
+		usuario.Senha,
+	)
+
+	if erro != nil {
+		return 0, erro
+	}
+	
+	ultimoIDInserido, erro := resultado.LastInsertId()
+	if erro != nil {
+		return 0, erro
+	}
+
+	return uint64(ultimoIDInserido), nil
 }
